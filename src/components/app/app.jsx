a3a156ee3e70ff058d9f173013004b2581e267ce
LAlex <trewstaples@gmail.com>
2022-08-01 17:16:50 +0300

    Добавит комопннет Form и сделает select

---
+++ src/components/app/app.jsx
@@ -0,0 +1,22 @@
+import { Form } from '../form'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+      <body>
+        <Form />
+      </body>
+    </div>
+  )
+}
+
+export { App }
+
+//Сделать кмпонент формы!
+//Сделать селект!
+//Сделать textarea
+//Добавить checkbox отображения
+//Добавить кнопку добавления text-кнопок
+//Добавить кнопку добавления url-кнопок
+//Написать данные для кнопок ?
---
+++ src/components/form/form.jsx
@@ -0,0 +1,15 @@
+const Form = () => {
+  return (
+    <form>
+      <select>
+        <option value=''>Выберите канал</option>
+        <option value='VK'>Вконтакте</option>
+        <option value='WhatsApp'>WhatsApp</option>
+        <option value='Telegram'>Telegram</option>
+        <option value='SMS'>SMS</option>
+      </select>
+    </form>
+  )
+}
+
+export { Form }
