e350fb4286618d62e2dc0bf9e432db34f4ee1f56
LAlex <trewstaples@gmail.com>
2022-08-02 15:14:41 +0300

    Добавит пробные кнопки в textarea

--- src/components/textarea/textarea.jsx
+++ src/components/textarea/textarea.jsx
@@ -1,3 +1,5 @@
+import './textarea.css'
+
 const textAreaMaxLength = {
   VK: '4096',
   WhatsApp: '10',
@@ -5,14 +7,27 @@ const textAreaMaxLength = {
   SMS: '',
 }
 
+let lastButtonWidth = 0
+
+let buttonStartPosition = {
+  left: lastButtonWidth,
+}
+
 const TextArea = ({ channel }) => {
   return (
-    <div className='main__text-wrap'>
-      <textarea
-        className='main__text'
-        autoComplete='off'
-        maxLength={textAreaMaxLength[channel]}
-      ></textarea>
+    <div className='main__textarea-wrap'>
+      <textarea maxLength={textAreaMaxLength[channel]} name={'foo'}>
+        Введите сообщение...
+      </textarea>
+      <div className='main__textarea-buttons-wrap'>
+        <button style={{ position: 'absolute', bottom: 0, left: lastButtonWidth }}>Menu</button>
+        <button style={{ position: 'absolute', bottom: 0, left: lastButtonWidth + '56.5469px' }}>
+          PlaceholdeButton
+        </button>
+        <button style={{ position: 'absolute', bottom: 0, left: 150 }}>PlaceholdeButton</button>
+        <button style={{ position: 'absolute', bottom: 0, left: 300 }}>PlaceholdeButton</button>
+        <button style={{ position: 'absolute', bottom: 0, left: 400 }}>PlaceholdeButton</button>
+      </div>
     </div>
   )
 }
